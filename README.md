071878ed9e58e24899967732512c0b7e52958c74
Tanner <tmartzdc@gmail.com>
2023-02-10 19:44:06 +0000

    v2.10.1

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # CMPS 2200  Recitation 02
 
-**Name (Team Member 1):**_________________________  
-**Name (Team Member 2):**_________________________
+**Name (Team Member 1):**Tanner Martz
+[v2.10.1]
 
 In this recitation, we will investigate recurrences. 
 To complete this recitation, follow the instructions in this document. Some of your answers will go in this file, and others will require you to edit `main.py`.
@@ -49,11 +49,22 @@ where $W(1) = 1$.
 - [ ] 4. (2 point) Now, derive the asymptotic behavior of $W(n)$ using $f(n) = 1$, $f(n) = \log n$ and $f(n) = n$. Then, generate actual values for $W(n)$ for your code and confirm that the trends match your derivations.
 
 **TODO: your answer goes here**
+The code I generated confirms derivations of the behaviors of above functions.
+- If f(n) = 1 as the lim n approaches infinity 1/W(n) allows f = O(log n).
+- If f(n) = log n as the lim n approaches infinity log n/W(n) = 0 allows f = O(log^2 n).
+- If f(n) = n as the lim n approaches infinity n/W(n) = 0 allows f = O(n log n).
+By testing the values of these functions we confirm the derivations.
 
 - [ ] 5. (4 points) Now that you have a nice way to empirically generate valuess of $W(n)$, we can look at the relationship between $a$, $b$, and $f(n)$. Suppose that $f(n) = n^c$. What is the asypmptotic behavior of $W(n)$ if $c < \log_b a$? What about $c > \log_b a$? And if they are equal? Modify `compare_work` to compare empirical values for different work functions (at several different values of $n$) to justify your answer. 
 
 **TODO: your answer goes here**
+By modifying compare_work function we can compare the values for the work functions. The correlation between c and log_b a indicates the rate of tree work and the rate of f(n) work. If c is less than log_b a, then b^c is less than a, and the rate of tree work is lower than the rate of f(n) work. W(n) is then characterized as Root-dominated. Conversely, if c is greater than log_b a, b^c is greater than a, leading to an increased rate of tree work and a W(n) classification of Leaf-dominated. However, if c is equal to log_b a, then b^c is equal to a, resulting in a balanced rate between tree work and f(n) work, and W(n) being considered Balanced. This is proven by the values generated in the compare_work function.
 
 - [ ] 6. (3 points) $W(n)$ is meant to represent the running time of some recursive algorithm. Suppose we always had $a$ processors available to us and we wanted to compute the span of the same algorithm. Implement the function `span_calc` to compute the empirical span, where the work of the algorithm is given by $W(n)$. Implement `test_compare_span` to create a new comparison function for comparing span functions. Derive the asymptotic expressions for the span of the recurrences you used in problem 4 above. Confirm that everything matches up as it should. 
 
 **TODO: your answer goes here**
+After implementing span_calc and test_compare_span functions accordingly, we continue from the recurrences used in problem 4:
+- If f(n) = 1 then lim n approaches infinity 1/S(n) allows f = O(log n)
+- If f(n)= log n then lim n approaches infinity log n/S(n)0 allows f = O(log^2 n)
+- If f(n)= n then lim n approaches infinity n/S(n) allow f = O(n log n)
+Everything matches up as it should.
--- main.py
+++ main.py
@@ -18,13 +18,21 @@ def simple_work_calc(n, a, b):
 	Returns: the value of W(n).
 	"""
 	# TODO
-	pass
+	if n == 1:
+	 return a * 1 - n
+	else:
+	 return a * simple_work_calc((n // b), a, b) + n
 
 def test_simple_work():
 	""" done. """
-	assert work_calc(10, 2, 2) == #TODO
-	assert work_calc(20, 3, 2) == #TODO
-	assert work_calc(30, 4, 2) == #TODO
+	assert simple_work_calc(10, 2, 2) == 36
+	assert simple_work_calc(20, 3, 2) == 311
+	assert simple_work_calc(30, 4, 2) == 1162
+
+	# 3 added tests
+	assert simple_work_calc(40, 2, 2) == 224
+	assert simple_work_calc(50, 3, 2) == 1124
+	assert simple_work_calc(60, 4, 2) == 4708
 
 def work_calc(n, a, b, f):
 	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
@@ -33,13 +41,15 @@ def work_calc(n, a, b, f):
 	n......input integer
 	a......branching factor of recursion tree
 	b......input split factor
-	f......a function that takes an integer and returns 
-           the work done at each node 
+	f......a function that takes an integer and returns the work done at each node 
 
 	Returns: the value of W(n).
 	"""
 	# TODO
-	pass
+	if n == 1:
+	 return a * 1 - f(n)
+	else:
+	 return a * work_calc((n // b), a, b, f) + f(n)
 
 def span_calc(n, a, b, f):
 	"""Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
@@ -54,15 +64,23 @@ def span_calc(n, a, b, f):
 	Returns: the value of W(n).
 	"""
 	# TODO
-	pass
+	if n == 1:
+	 return a * 1 - f(n)
+	else:
+	 return work_calc((n // b), a, b, f) // a + f(n)
 
 def test_work():
 	""" done. """
-	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
-	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
-	assert work_calc(30, 3, 2, lambda n: n) == #TODO
+	assert work_calc(10, 2, 2,lambda n: 1) == 15
+	assert work_calc(20, 1, 2, lambda n: n*n) == 529
+	assert work_calc(30, 3, 2, lambda n: n) == 381
 
-def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
+	# 3 added work tests
+	assert work_calc(40, 2, 2,lambda n: 1) == 63
+	assert work_calc(50, 1, 2, lambda n: n*n) == 3314
+	assert work_calc(60, 3, 2, lambda n: n) == 1203
+
+def compare_work(work_fn1, work_fn2, input_sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
 	"""
 	Compare the values of different recurrences for 
 	given input sizes.
@@ -91,13 +109,43 @@ def print_results(results):
 
 def test_compare_work():
 	# curry work_calc to create multiple work
-	# functions taht can be passed to compare_work
-    
+	# functions that can be passed to compare_work
+
 	# create work_fn1
 	# create work_fn2
+	# create work_fn3
+	work_fn1 = lambda n: work_calc(n, 3, 2, lambda n: 1)
+	work_fn2 = lambda n: work_calc(n, 3, 2, lambda n: n)
+	work_fn3 = lambda n: work_calc(n, 3, 2, lambda n: n*n)
 
-    res = compare_work(work_fn1, work_fn2)
-	print(res)
+	# print compare work test 1
+	print("compare fn1 with fn2")
+	res = compare_work(work_fn1, work_fn2)
+	print_results(res)
+
+	# print compare work test 2
+	print("compare fn1 with fn3")
+	res = compare_work(work_fn1, work_fn3)
+	print_results(res)
+
+	# print compare work test 3
+	print("compare fn3 with fn2")
+	res = compare_work(work_fn3, work_fn2)
+	print_results(res)
+
+def print_results_span(results):
+	print(tabulate.tabulate(results,
+							headers=['1', '2', '4', '8', '16'],
+							floatfmt=".3f",
+							tablefmt="github"))
 
 def test_compare_span():
 	# TODO
+	n_list = list()
+	for n in [10, 20, 50, 100, 1000, 5000, 10000]:
+	 p_list = [n]
+	for processor in [1, 2, 4, 8, 16]:
+	 s = span_calc(n, processor, 2, lambda n: n)
+	 p_list.append(s)
+	 n_list.append(p_list)
+	print_results_span(n_list)
